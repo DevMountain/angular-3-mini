5a545e4b690521cd364ddc0deb8f6a03af84f2ec
= <webdev.jameslemire@gmail.com>
2017-09-25 10:49:30 -0600

    JL solution

--- app/header/header.component.js
+++ app/header/header.component.js
@@ -1,7 +1,3 @@
-/*  This component is to show you can pass things down through bindings, should we keep it structured this way
-    or would you rather I create another anonymus function for a controller and assign a `this.title`?
-*/
-
 angular.module('fourWheels').component('header', {
   templateUrl: 'app/header/header.template.html',
   controllerAs: 'headerCtrl',
